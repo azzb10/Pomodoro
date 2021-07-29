3ba633c1f4f426a2f0111db471e57c19d4f2eac7
alissondemidio <alissond1995@gmail.com>
2021-07-29 02:43:44 -0200

    Update assets

---
+++ src/assets/assets.js
@@ -0,0 +1,6 @@
+export default {
+  gif1: require('./64627-time-management-lottie-animation.json'),
+  gif2: require('./36963-working.json'),
+  gif3: require('./36963-working.json'),
+  gif4: require('./42410-sleeping-polar-bear.json'),
+};
--- src/pages/Working/index.js
+++ src/pages/Working/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import assets from '../../assets/assets';
 import Countdown from '../../components/Countdown';
+import LottieComponent from '../../components/LottieComponent';
 
 const Working = () => {
+  const item = assets.gif2;
   return (
     <View style={styles.container}>
-      <Text>Working</Text>
+      <View style={styles.pageHeader}>
+        <Text style={styles.title}>Working</Text>
+        <Text style={styles.subtitle}>
+          Focus on the work until the clock stops
+        </Text>
+      </View>
       <View style={styles.countBackground}>
+        <View style={styles.animation}>
+          <LottieComponent item={item} />
+        </View>
         <Countdown valueMinutes={25} valueSeconds={0} />
       </View>
     </View>
@@ -16,17 +27,35 @@ const Working = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ddd',
+    backgroundColor: '#8feea1',
+  },
+  pageHeader: {
+    margin: 30,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#fff',
   },
   countBackground: {
     width: '90%',
     height: '60%',
     backgroundColor: '#fff',
     alignSelf: 'center',
+    alignItems: 'center',
     padding: 20,
     margin: 60,
     borderRadius: 20,
     justifyContent: 'space-between',
+  },
+  animation: {
+    height: '60%',
+    width: '100%',
   },
 });
 
