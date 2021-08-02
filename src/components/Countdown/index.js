c37220027fef06488ac8370bdd9dbf9d2151f988
alissondemidio <alissond1995@gmail.com>
2021-08-02 13:07:53 -0200

    Update Countdown Components

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,43 +1,29 @@
-/* eslint-disable no-shadow */
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import CountDown from 'react-native-countdown-component';
 
 const Countdown = ({valueMinutes, valueSeconds}) => {
-  const [seconds, setSeconds] = useState(valueSeconds);
-  const [minutes, setMinutes] = useState(valueMinutes);
-
-  const countdownSeconds = () => {
-    setSeconds(seconds => seconds - 1);
-  };
-
-  useEffect(() => {
-    setInterval(() => {
-      countdownSeconds();
-    }, 1000);
-  }, []);
-
-  if (seconds < 0) {
-    setSeconds(59);
-    if (minutes <= 0) {
-      setMinutes(25);
-    } else {
-      setMinutes(minutes => minutes - 1);
-    }
-  }
-
   return (
     <View>
-      <Text style={styles.counter}>
-        {minutes}:{('00' + seconds).slice(-2)}
-      </Text>
+      <CountDown
+        until={60 * 10 + 30}
+        size={30}
+        onFinish={() => alert('Finished')}
+        digitStyle={styles.counterBackground}
+        digitTxtStyle={styles.counterText}
+        timeToShow={['M', 'S']}
+        timeLabels={{m: 'MM', s: 'SS'}}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  counter: {
+  counterText: {
+    color: '#fff',
+  },
+  counterBackground: {
     color: '#000',
-    fontSize: 70,
   },
 });
 
