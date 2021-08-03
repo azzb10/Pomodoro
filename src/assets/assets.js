895a49fc74196aece10bbaaa813c753dfbcdffc3
alissondemidio <alissond1995@gmail.com>
2021-08-03 01:13:08 -0200

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,4 +3,5 @@ export default {
   gif2: require('./36963-working.json'),
   gif3: require('./45551-men-relaxing-on-working-chair.json'),
   gif4: require('./42410-sleeping-polar-bear.json'),
+  gif5: require('./7184-done.json'),
 };
